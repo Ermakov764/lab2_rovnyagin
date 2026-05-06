a930a2b5e34f45931b8e484f141c7a6739f2909e
serafim <ermak.lavr764@gmail.com>
2026-05-06 12:04:17 +0300

    fix: Spring Boot 4 Kafka через spring-boot-starter-kafka и @EnableKafka

--- src/main/java/ru/hse/lab2/Lab2Application.java
+++ src/main/java/ru/hse/lab2/Lab2Application.java
@@ -2,9 +2,11 @@ package ru.hse.lab2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableKafka
 @EnableScheduling
 public class Lab2Application {
     public static void main(String[] args) {
